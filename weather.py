b4309ff1e2ad4a900aa4a4d635d5dc5cd3a79333
Norgius <dmitry.schadrin21@yandex.ru>
2022-09-07 21:08:41 +0300

    Second version

--- weather.py
+++ weather.py
@@ -1,9 +1,10 @@
 import requests
 
 cities = ('Лондон', 'Шереметьево', 'Череповец')
-url_template = 'https://wttr.in/{}?nTqM&lang=ru'
+url_template = 'https://wttr.in/{}'
+params_request = {'MnTq': '', 'lang': 'ru'}
 
 for city in cities:
-    response = requests.get(url_template.format(city))
+    response = requests.get(url_template.format(city), params=params_request)
     response.raise_for_status()
     print(response.text)
