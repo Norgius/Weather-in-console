61f768300bae14c279e432a90d6a69bdd9e9ca21
Norgius <dmitry.schadrin21@yandex.ru>
2022-09-08 20:20:57 +0300

    Changed the variable name 'request_params'

--- weather.py
+++ weather.py
@@ -2,9 +2,9 @@ import requests
 
 cities = ('Лондон', 'Шереметьево', 'Череповец')
 url_template = 'https://wttr.in/{}'
-params_request = {'MnTq': '', 'lang': 'ru'}
+request_params = {'MnTq': '', 'lang': 'ru'}
 
 for city in cities:
-    response = requests.get(url_template.format(city), params=params_request)
+    response = requests.get(url_template.format(city), params=request_params)
     response.raise_for_status()
     print(response.text)
